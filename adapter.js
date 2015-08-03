609e6f60c325ffb24c7fd788e76bb8505f873425
LuisHsu <luis8404272@gmail.com>
2015-08-04 00:09:34 +0800

    Timer Finished

--- adapter.js
+++ adapter.js
@@ -16,3 +16,15 @@ if( isMobile.phone || isMobile.tablet){
 	mycss.href = "desktop.css";
 	head.appendChild(mycss);
 }
+
+function dateTimerTick(){
+	var openTime = new Date(2015,9,14,8,0,0,0);
+	var curTime = new Date();
+	var diff = new Date(openTime.getTime() - curTime.getTime() - 28800000);
+	document.getElementById("TimerText").innerHTML = "距離開學還有：" +
+		(diff.getMonth()-1)+"月"  +(diff.getDate()-1)   +"日"+
+		diff.getHours()+"小時"+diff.getMinutes()+"分"+
+		diff.getSeconds()+"秒";
+}
+
+setInterval(dateTimerTick,1000);
