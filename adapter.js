d48bf7ad6bc06cfad4ebed4096f4ab7e791b8d15
LuisHsu <luis8404272@gmail.com>
2015-08-18 20:16:52 +0800

    Adjust Map

--- adapter.js
+++ adapter.js
@@ -11,7 +11,7 @@ function dateTimerTick(){
 	var openTime = new Date(2015,9,14,8,0,0,0);
 	var curTime = new Date();
 	var diff = new Date(openTime.getTime() - curTime.getTime() - 28800000);
-	document.getElementById("TimerText").innerHTML = "距離開學還有：" +
+	document.getElementById("TimerText").innerHTML = "開學倒數：" +
 		(diff.getMonth()-1)+"月"  +(diff.getDate()-1)   +"日"+
 		diff.getHours()+"小時"+diff.getMinutes()+"分"+
 		diff.getSeconds()+"秒";
