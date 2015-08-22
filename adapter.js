c66bc9cd0f18904e74e024e1cefed50c80f2fab1
LuisHsu <luis8404272@gmail.com>
2015-08-22 12:29:24 +0800

    Test Fone 1

--- adapter.js
+++ adapter.js
@@ -2,7 +2,7 @@ var mode="desktop";
 
 if( isMobile.phone || isMobile.tablet){
 	mode="mobile";
-
+	
 }else{
 
 }
@@ -11,7 +11,7 @@ function dateTimerTick(){
 	var openTime = new Date(2015,9,14,8,0,0,0);
 	var curTime = new Date();
 	var diff = new Date(openTime.getTime() - curTime.getTime() - 28800000);
-	document.getElementById("TimerText").innerHTML = "開學倒數：" +
+	document.getElementById("TimerText").innerHTML = "開幕倒數：" +
 		(diff.getMonth()-1)+"月"  +(diff.getDate()-1)   +"日"+
 		diff.getHours()+"小時"+diff.getMinutes()+"分"+
 		diff.getSeconds()+"秒";
